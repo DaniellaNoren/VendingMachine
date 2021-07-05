64809375fb961f1a331e927789a65cde6aa7a07b
Daniella Noren <daniellanoren@live.se>
2021-07-05 13:45:11 +0200

    Add Test-project. Rename Program

---
+++ VendingMachine.Test/VendingMachineProgramTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VendingMachine.Test
+{
+    public class VendingMachineProgramTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- VendingMachine/VendingMachineProgram.cs
+++ VendingMachine/VendingMachineProgram.cs
@@ -2,7 +2,7 @@
 
 namespace VendingMachine
 {
-    class Program
+    class VendingMachineProgram
     {
         private static readonly int[] validAmounts = new int[] { 1, 5, 10, 20, 50, 100, 500, 1000 };
         private static int cost;
