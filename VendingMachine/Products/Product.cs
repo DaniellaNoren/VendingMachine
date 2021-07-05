9184e65ede70df150bfe58e6e44650e6f620cd38
Daniella Noren <daniellanoren@live.se>
2021-07-05 13:43:19 +0200

    Add project files.

---
+++ VendingMachine/Products/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    abstract public class Product
+    {
+        int price;
+        public int Price { get { return price; } set { price = value; } }
+
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        string description;
+        public string Description { get { return description; } set { description = value; } }
+        
+        public Product(string name, int price, string description)
+        {
+            this.Price = price;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public virtual string Examine()
+        {
+            return $"Type: {Name}, Cost: {Price}, Description: {Description}";
+        }
+
+        public abstract string Use();
+    }
+}
---
+++ VendingMachine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        private static readonly int[] validAmounts = new int[] { 1, 5, 10, 20, 50, 100, 500, 1000 };
+        private static int cost;
+        private static int moneyPool;
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
